1aa94ce35da24d3ddb219b82a6c7cf14b2a44957
Sanjay K <work@sanjay.link>
2022-12-08 11:53:10 +0530

    Add OperationalTransformationController
    Handle `/v1/operations/transform` endpoint

--- lib/app.rb
+++ lib/app.rb
@@ -1,9 +1,13 @@
 require 'sinatra/base'
 require 'json'
 
+require_relative 'controllers/operational_transformation_controller'
+
 class App < Sinatra::Base
   configure do
       enable :logging
   end
+
+  use OperationalTransformationController
   run!
 end
---
+++ lib/controllers/operational_transformation_controller.rb
@@ -0,0 +1,23 @@
+require 'sinatra/base'
+require_relative '../domain/document'
+require_relative '../service/operational_transformation_service'
+require_relative '../helpers/input_deserializer'
+require_relative '../helpers/error'
+
+class OperationalTransformationController < Sinatra::Base
+  def initialize(app = nil)
+    super(app)
+  end
+
+  post '/v1/operations/transform' do
+    request.body.rewind
+    deserializer = InputDeserializer.new(request)
+    document = Document.new(deserializer.stale)
+    operational_transformation_service = OperationalTransformationService.new(deserializer, document)
+
+    { "latest": operational_transformation_service.update_document }.to_json
+  rescue InvalidTransformation, KeyError, InvalidType => e
+    status 400
+    { "message": e.message }.to_json
+  end
+end
